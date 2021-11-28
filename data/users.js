8be6b7966ac5193b37151b4fb6833246d45ef7af
bansri01 <patelbansri987654321@gmail.com>
2021-11-28 15:39:41 -0500

    added checkUser, and images using multer

--- data/users.js
+++ data/users.js
@@ -220,10 +220,11 @@ async function updateUser(id, profilePicture, firstName, lastName, emailAddress,
     //     throw `Password should be atleast 8 characters long`
     // }
 
-    // const hashedPwd = await bcrypt.hash(password, saltRounds);
+    // const hashedPwd = await bcrypt.hash(password, saltRounds); 123-154-1245
 
-    let phoneRe = /^\d{3}\-\d{3}\-\d{4}$/
-    if(!phoneNumber.match(phoneRe)) throw `Phone number must be of format xxx-xxx-xxxx and all numbers`
+    // let phoneRe = /^\d{3}\-\d{3}\-\d{4}$/
+    let phoneRe = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    if(!phoneNumber.match(phoneRe)) throw `Phone number must be all numbers and correct format`
 
     const countryCodes = Object.keys(countries.countries);
     const countryNames = countryCodes.map(code => countries.countries[code].name);
@@ -281,10 +282,57 @@ async function updateUser(id, profilePicture, firstName, lastName, emailAddress,
     return {userUpdated: true}
 }
 
+async function checkUser(username, password){
+    if(!username || !password){
+        throw `All fields need to have valid values`
+    }
+
+    if(typeof username !== 'string' || /^ *$/.test(username)){
+        throw `Please enter a valid string`
+    }
+
+    if(/[^A-Za-z0-9]/g.test(username)){
+        throw `Username should only have numbers and alphabets`
+    }
+
+    if(username.length < 4){
+        throw `Username should have atleast 4 characters`
+    }
+
+    if(/^ *$/.test(password)) throw `password cannot be empty`
+
+    if(/\s/g.test(password)) throw `password cannot have spaces`
+
+    if(password.length < 8){
+        throw `Password should be atleast 8 characters long`
+    }
+
+    const userCollection = await users()
+    const user = await userCollection.findOne({ username: username.toLowerCase()})
+
+    if(user === null) throw `Either the username or password is invalid`;
+
+    let compareToMatch = false;
+
+    try {
+        compareToMatch = await bcrypt.compare(password, user.password);
+    } catch (e) {
+        //no op
+    }
+
+    if (compareToMatch) {
+        return {authenticated: true}
+    } else {
+        throw `Either the username or password is invalid`
+    }
+
+}
+
 module.exports = {
     createUser,
     get,
-    updateUser
+    updateUser,
+    checkUser
 }
 
 
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,26 @@ const userData = data.users;
 const userColl = mongoCollections.users;
 const ObjectID  = require('mongodb').ObjectId;
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/images/users")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+var upload = multer({ storage: storage })
+
+router.post('/profile', upload.single('profile'), (req, res) => {
+    try {
+      res.send(req.file);
+    }catch(err) {
+      res.send(400);
+    }
+  });
+
+  module.exports= router;
+
+
 
