1d5bb49358e5c00db3ad5949ceab2420700094ba
Pallavi Jaiswal <pallujaiswal0@gmail.com>
2021-12-01 03:34:32 -0400

    SignUp router error handling finished

--- data/users.js
+++ data/users.js
@@ -51,76 +51,85 @@ function validateDate(date) {
 
 async function createUser(profilePicture, firstName, lastName, username, emailAddress, password, phoneNumber, country, biography, gender, userType, dateOfBirth){
     if(!profilePicture || !firstName || !lastName || !username || !emailAddress || !password || !phoneNumber || !country || !biography || !gender || !userType || !dateOfBirth){
-        throw `All fields must be supplied`
+        throw {message: `All fields must be supplied`, error:400}
     }
 
-    if(typeof firstName !== "string") throw `firstName must be string`
-    if(typeof lastName !== "string" ) throw `lastName must be string`
-    if(typeof emailAddress !== "string") throw 'emailAddress must be string'
-    if(typeof password !== "string") throw 'password must be string'
-    if(typeof country !== "string") throw 'country must be string'
-    if(typeof biography !== "string") throw 'biography must be string'
-    if(typeof gender !== "string") throw 'gender must be string'
-    if(typeof userType !== "string") throw 'userType must be string'
-    if(typeof phoneNumber !== "string") throw 'phoneNumber must be string'
-    if(typeof dateOfBirth !== "string") throw 'dateOfBirth must be string'
-    if(typeof username !== "string") throw `userName must be string`
+    if(typeof firstName !== "string") throw {message:`firstName must be string`,error:400}
+    if(typeof lastName !== "string" ) throw {message:`lastName must be string`,error:400}
+    if(typeof emailAddress !== "string") throw {message:'emailAddress must be string',error:400}
+    if(typeof password !== "string") throw {message:'password must be string',error:400}
+    if(typeof country !== "string") throw {message:'country must be string',error:400}
+    if(typeof biography !== "string") throw {message:'biography must be string',error:400}
+    if(typeof gender !== "string") throw {message: 'gender must be string',error:400}
+    if(typeof userType !== "string") throw {message:'userType must be string',error:400}
+    if(typeof phoneNumber !== "string") throw {message:'phoneNumber must be string',error:400}
+    if(typeof dateOfBirth !== "string") throw {message:'dateOfBirth must be string',error:400}
+    if(typeof username !== "string") throw {message:`userName must be string`,error:400}
 
-    if (/^ *$/.test(firstName)) throw `firstName cannot be empty`
-    if (/^ *$/.test(lastName)) throw `lastName cannot be empty`
-    if (/^ *$/.test(emailAddress)) throw `emailAddress cannot be empty`
-    if (/^ *$/.test(password)) throw `password cannot be empty`
-    if (/^ *$/.test(country)) throw `country cannot be empty`
-    if (/^ *$/.test(biography)) throw `biography cannot be empty`
-    if (/^ *$/.test(gender)) throw `gender cannot be empty`
-    if (/^ *$/.test(userType)) throw `userType cannot be empty`
-    if (/^ *$/.test(phoneNumber)) throw `phoneNumber cannot be empty`
-    if (/^ *$/.test(dateOfBirth)) throw `dateOfBirth cannot be empty`
-    if (/^ *$/.test(username)) throw `Username cannot be empty`
+    if (/^ *$/.test(firstName)) throw {message:`firstName cannot be empty`,error:400}
+    if (/^ *$/.test(lastName)) throw {message:`lastName cannot be empty`,error:400}
+    if (/^ *$/.test(emailAddress)) throw {message:`emailAddress cannot be empty`,error:400}
+    if (/^ *$/.test(password)) throw {message:`password cannot be empty`,error:400}
+    if (/^ *$/.test(country)) throw {message:`country cannot be empty`,error:400}
+    if (/^ *$/.test(biography)) throw {message:`biography cannot be empty`,error:400}
+    if (/^ *$/.test(gender)) throw {message:`gender cannot be empty`,error:400}
+    if (/^ *$/.test(userType)) throw {message:`userType cannot be empty`,error:400}
+    if (/^ *$/.test(phoneNumber)) throw {message:`phoneNumber cannot be empty`,error:400}
+    if (/^ *$/.test(dateOfBirth)) throw {message:`dateOfBirth cannot be empty`,error:400}
+    if (/^ *$/.test(username)) throw {message:`Username cannot be empty`,error:400}
 
     if(/[^A-Za-z0-9]/g.test(username)){
-        throw `Username should only have numbers and alphabets`
+        throw {message:`Username should only have numbers and alphabets`,error:400}
     }
 
     if(username.length < 4){
-        throw `Username should have atleast 4 characters`
+        throw {message:`Username should have atleast 4 characters`,error:400}
     }
 
-    if(!validateEmail(emailAddress)) throw `Please Enter valid Email Address`
+    if(!validateEmail(emailAddress)) throw {message:`Please Enter valid Email Address`,error:400}
 
-    if(/\s/g.test(password)) throw `password cannot have spaces`
+    if(/\s/g.test(password)) throw {message: `password cannot have spaces`,error:400}
     if(password.length < 8){
-        throw `Password should be atleast 8 characters long`
+        throw {message:`Password should be atleast 8 characters long`,error:400};
     }
 
     const hashedPwd = await bcrypt.hash(password, saltRounds);
-
+    
     let phoneRe = /^\d{3}\-\d{3}\-\d{4}$/
-    if(!phoneNumber.match(phoneRe)) throw `Phone number must be of format xxx-xxx-xxxx and all numbers`
+    if(!phoneNumber.match(phoneRe)) throw {message:`Phone number must be of format xxx-xxx-xxxx and all numbers`, error: 400};
   
     const countryCodes = Object.keys(countries.countries);
     const countryNames = countryCodes.map(code => countries.countries[code].name);
-    if (!countryNames.includes(country)) throw `Please enter a valid country`
+    
+    if (!countryNames.includes(country)) throw {message:`Please enter a valid country`, error: 400};
 
 
     let gen = ["Female", "Male", "other"]
 
     // if (gender !== "Female" || gender !== "Male" || gender !== "other") throw `Gender must be Male or Female or other`
-    if(!gen.includes(gender))
-
-    if (userType !== "Patient" || userType !== "Doctor") throw `Usertype must be a patient or a doctor`
-
-    if (!validateDate(dateOfBirth)) throw `Please Enter valid date of birth`
+    
+    if(!gen.includes(gender)) throw {message:`Please enter valid gender`, error: 400};
+    
+    if (userType !== "Patient" && userType !== "Doctor") throw {message:`Usertype must be a patient or a doctor`, error: 400};
+    try{
+    if (!validateDate(dateOfBirth)) throw {
+        message:`Please Enter valid date of birth`, error: 400};
+    }
+    catch(e){
+        throw {message:e, error: 400};
+    }
 
     const userCollection = await users()
 
     const lowerUser = emailAddress.toLowerCase()
     const userexists = await userCollection.findOne({ emailAddress: lowerUser})
-    if(userexists) throw `User with that email address already exists`
+   
+    if(userexists) throw  {message:`User with that email address already exists`, error: 400};
 
     const lowerUsername = username.toLocaleLowerCase()
     const usertaken = await userCollection.findOne({ username: lowerUsername})
-    if(usertaken) throw `Username already taken`
+    
+    if(usertaken) throw {message:`Username already taken`, error: 400};
 
 
     let newUser = {
@@ -139,7 +148,7 @@ async function createUser(profilePicture, firstName, lastName, username, emailAd
     }
 
     const insertInfo = await userCollection.insertOne(newUser)
-    if (insertInfo.insertCount == 0) throw `Could not add user`
+    if (insertInfo.insertCount == 0) throw {message:`Could not add user`,error:400}
 
     const newId = insertInfo.insertedId.toString();
     const user = await get(newId);
--- routes/users.js
+++ routes/users.js
@@ -7,51 +7,66 @@ const mongoCollections = require('../config/mongoCollections')
 const userColl = mongoCollections.users;
 const ObjectID  = require('mongodb').ObjectId;
 const router = express.Router();
+const countries = require("countries-list");
 
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "public/images/users")
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + '-' + Date.now())
-//     }
-// })
 
-// var upload = multer({ storage: storage })
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
-// router.post('/profile', upload.single('profile'), (req, res) => {
-//     try {
-//       res.send(req.file);
-//     }catch(err) {
-//       res.send(400);
-//     }
-//   });
+function validateDate(date) {
+        let datePattern = /^\d{4}-\d{2}-\d{2}$/ 
+            if(!date.match(datePattern)) throw `dateOfReview should be in format yyyy-mm-dd`
+            const today = new Date()
+            let date_arr = date.split("-")
+            parsedMonth = Number(date_arr[1])
+            parsedDay = Number(date_arr[2])
+            parsedYear = Number(date_arr[0])
+            if(parsedMonth < 1 || parsedMonth > 12){
+                throw `Invalid month`
+            }
+            if(parsedDay < 1 || parsedDay > 31){
+                throw 'Invalid Day value'
+            }
+            monthArr1 = [1, 3, 5, 7, 8, 10, 12]
+            monthArr2 = [4, 6, 9, 11]
+            if(monthArr1.includes(parsedMonth) && parsedDay > 31){
+                throw `The month does not have more than 31 days`
+            }
+            else if(monthArr2.includes(parsedMonth) && parsedDay > 30){
+                throw `The month does not have more than 30 days`
+            }
+            else if(parsedMonth === 2 && parsedDay > 28 ){
+                throw `The month february does not have more than 28 days`
+            }
+            
+            let d1 = new Date(Date.parse(date));
+            let d2 = new Date(Date.parse(today));
+            var diff = d2.getTime() - d1.getTime();
+    
+            if (diff < 0) {
+                return false; 
+            } else {
+                return true;
+            }
+        
+    }
 
-// router.get('/profile', async (req, res) => {
-//     // const userdata = await getbyUsername(req.session.user)
-//     const userdata = await usersData.getByUsername("user01")
-//     res.render("users/userProfile", {firstname: userdata.firstName, lastname: userdata.lastName, biography: userdata.biography, gender: userdata.gender, phoneNumber: userdata.phoneNumber, emailAddress: userdata.emailAddress, location: userdata.country})
-// })
+
+
+
 
 router.get('/',async (req, res) => {
     if (req.session.user) {
         return res.redirect('/private');
       } else {
         res.render('homepage/Landingpage',{title: "Share and Care"});
-       // res.render("users/login",{title: "LOGIN"});
+       
       }
     
   });
-//   router.get('/',async (req, res) => {
-//     res.render('Users/Login');
-//   });
-  
-//   router.post('/signup',async (req, res) => {
-//     const postSignup = await usersData.createUser(req.body.username,req.body.password);
-//     if(postSignup.userInserted){
-//         return res.redirect('/');
-//     }
-//   });
+
 
 router.get('/Login',async (req, res) => {
     res.render("users/Login");      
@@ -118,43 +133,159 @@ router.get('/signup',async (req, res) => {
         res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide Date of Birth'});
         return;
     }
-    if(typeof firstName !== "string") {
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'firstName must be string'});
+    if(typeof req.body.firstName !== "string") {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Firstname must be string'});
         return; 
     }
-    if(typeof lastName !== "string" ) {
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'lastName must be string'});
+    if(typeof req.body.lastName !== "string" ) {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Lastname must be string'});
         return; 
     }
-    if(typeof emailAddress !== "string"){
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'emailAddress must be string'});
+    if(typeof req.body.emailAddress !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'EmailAddress must be string'});
         return; 
     }
-    if(typeof password !== "string"){
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'password must be string'});
+    if(typeof req.body.password !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Password must be string'});
         return;   
     }
-    if(typeof country !== "string"){
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'country must be string'});
+    if(typeof req.body.country !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Country must be string'});
         return; 
     }
-    if(typeof biography !== "string"){
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'biography must be string'});
+    if(typeof req.body.biography !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Biography must be string'});
         return;
     }
-    if(typeof gender !== "string"){
-        res.status(400).render('users/signup',{ title:"SignUp",error: 'gender must be string'});
+    if(typeof req.body.gender !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Gender must be string'});
         return; 
     }
-    if(typeof userType !== "string") throw 'userType must be string'
-    if(typeof phoneNumber !== "string") throw 'phoneNumber must be string'
-    if(typeof dateOfBirth !== "string") throw 'dateOfBirth must be string'
-    if(typeof username !== "string") throw `userName must be string`
-     
-    const user_data = req.body;
-    console.log(user_data)
+    if(typeof req.body.userType !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'UserType must be string'});
+        return; 
+    }
+    if(typeof req.body.phoneNumber !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'PhoneNumber must be string'});
+        return; 
+    }
+    if(typeof req.body.dateOfBirth !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'DateOfBirth must be string'});
+        return; 
+    }
+    if(typeof req.body.username !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'UserName must be string'});
+        return; 
+    }
+
+    if (/^ *$/.test(req.body.firstName)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Firstname cannot be empty'});
+        return; 
+    }
+    if (/^ *$/.test(req.body.lastName)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Lastname cannot be empty'});
+        return; 
+    }
+    if (/^ *$/.test(req.body.emailAddress)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'EmailAddress cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.password)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Password cannot be empty'});
+        return; 
+    }
+    if (/^ *$/.test(req.body.country)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Country cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.biography)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Biography cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.gender)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Gender cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.userType)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'UserType cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.phoneNumber)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'PhoneNumber cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.dateOfBirth)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'DateOfBirth cannot be empty'});
+        return;
+    }
+    if (/^ *$/.test(req.body.username)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Username cannot be empty'});
+        return;
+    }
+
+
+    if(/[^A-Za-z0-9]/g.test(req.body.username)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Username should only have numbers and alphabets'});
+        return;
+    }
+
+    if(req.body.username.length < 4){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Username should have atleast 4 characters'});
+        return;
+    }
+    
+    if(!validateEmail(req.body.emailAddress)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Please Enter valid Email Address'});
+        return;
+    }
+
+    if(/\s/g.test(req.body.password)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'password cannot have spaces'});
+        return;  
+    }
+    if(req.body.password.length < 8){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Password should be atleast 8 characters long'});
+        return; 
+    }
+    let phoneRe = /^\d{3}\-\d{3}\-\d{4}$/
+    if(!req.body.phoneNumber.match(phoneRe)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Phone number must be of format xxx-xxx-xxxx and all numbers'});
+        return
+    }
+    const countryCodes = Object.keys(countries.countries);
+    const countryNames = countryCodes.map(code => countries.countries[code].name);
+    if (!countryNames.includes(req.body.country)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Please enter a valid country'});
+        return
+    }
+    let gen = ["Female", "Male", "other"]
+    if(!gen.includes(req.body.gender)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Please enter valid gender'});
+        return
+    }
+
+
+    if (req.body.userType.trim() != "Patient" && req.body.userType.trim() != "Doctor"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Usertype must be a patient or a doctor'});
+        return  
+    }
+    
+     try{ 
+    if (!validateDate(req.body.dateOfBirth)){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'Please Enter valid date of birth'});
+        return
+    }
+}
+catch(e){
+    res.status(400).render('users/signup',{ title:"SignUp",error: e});
+    return
+
+}
+
+   
 
     try{
+        const user_data = req.body;
         const { profilePicture, firstName, lastName, username, emailAddress, password, phoneNumber, country, biography, gender, userType, dateOfBirth} = user_data;
         const postSignup = await usersData.createUser(profilePicture, firstName, lastName, username, emailAddress, password, phoneNumber, country, biography, gender, userType, dateOfBirth);
             if(postSignup){
@@ -162,8 +293,7 @@ router.get('/signup',async (req, res) => {
             }
         }
     catch(e){
-        res.status(400).render("users/signup");
-        //res.status().render('users/signup',{title:"SignUp",error: e.message||`Internal Server Error`})
+        res.status(e.error || 500).render('users/signup',{title:"SignUp",error: e.message ||`Internal Server Error`})
     }
   })
 
