a082e8540fb73b010cdeecbb14841e1197857c0c
Pallavi Jaiswal <pallujaiswal0@gmail.com>
2021-12-01 00:15:31 -0400

    routes error handling for signup

--- routes/users.js
+++ routes/users.js
@@ -54,17 +54,99 @@ router.get('/',async (req, res) => {
 //   });
 
 router.get('/Login',async (req, res) => {
-    res.render("Users/Login");      
+    res.render("users/Login");      
 });
 
 router.get('/signup',async (req, res) => {
     if (req.session.user) {
         return res.redirect('/private');
       } else {
-        res.render("Users/signup",{title: "SIGNUP"});
+        res.render("users/signup",{title: "SIGNUP"});
       }     
   });
   router.post('/signup',async (req, res) => {
+
+    if (!req.body.profilePicture )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide Profile picture'});
+        return;
+      }
+
+    if (!req.body.firstName )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide First Name'});
+        return;
+      }
+    if (!req.body.lastName )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Last Name'});
+        return;
+      }
+    if (!req.body.username )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Username'});
+        return;
+      }  
+    if (!req.body.emailAddress )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Email Address'});
+        return;
+      } 
+    if (!req.body.password )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Password'});
+        return;
+      } 
+    if (!req.body.phoneNumber )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Phone Number'});
+        return;
+      } 
+    if (!req.body.country )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Country'});
+        return;
+    }
+    if (!req.body.biography )  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide profile Biography'});
+        return;
+    }
+    if (!req.body.gender)  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide Gender'});
+        return;
+    }
+    if (!req.body.userType)  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide UserType'});
+        return;
+    }
+    if (!req.body.dateOfBirth)  {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'You must provide Date of Birth'});
+        return;
+    }
+    if(typeof firstName !== "string") {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'firstName must be string'});
+        return; 
+    }
+    if(typeof lastName !== "string" ) {
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'lastName must be string'});
+        return; 
+    }
+    if(typeof emailAddress !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'emailAddress must be string'});
+        return; 
+    }
+    if(typeof password !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'password must be string'});
+        return;   
+    }
+    if(typeof country !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'country must be string'});
+        return; 
+    }
+    if(typeof biography !== "string"){
+        res.status(400).render('users/signup',{ title:"SignUp",error: 'biography must be string'});
+        return;
+    }
+    if(typeof gender !== "string") throw 'gender must be string'{
+        
+    }
+    if(typeof userType !== "string") throw 'userType must be string'
+    if(typeof phoneNumber !== "string") throw 'phoneNumber must be string'
+    if(typeof dateOfBirth !== "string") throw 'dateOfBirth must be string'
+    if(typeof username !== "string") throw `userName must be string`
+
     try{
         const { profilePicture, firstName, lastName, username, emailAddress, password, phoneNumber, country, biography, gender, userType, dateOfBirth} = blogPostData;
         const postSignup = await usersData.createUser(profilePicture, firstName, lastName, username, emailAddress, password, phoneNumber, country, biography, gender, userType, dateOfBirth);
@@ -73,7 +155,7 @@ router.get('/signup',async (req, res) => {
             }
         }
     catch(e){
-        res.status(400).render("Users/signup");
+        res.status(400).render("users/signup");
         //res.status().render('users/signup',{title:"SignUp",error: e.message||`Internal Server Error`})
     }
   })
@@ -81,14 +163,14 @@ router.get('/signup',async (req, res) => {
   router.get('/private',async (req, res) => {
     
     if (req.session.user) {
-        return res.render("Users/private",{title:"Private", username: req.session.user})
+        return res.render("users/private",{title:"Private", username: req.session.user})
       }
     });
 
 
   router.get('/logout',async (req, res) => {
     req.session.destroy();
-    return res.render("Users/logout",{title:"Logout"})
+    return res.render("users/logout",{title:"Logout"})
     
 
 });
