496c4655383b0d070b0bf135d55b07b8edb8bdd5
bhavya <bshah24@stevens.edu>
2021-12-02 17:39:00 -0500

    Fixing minor Code Errors

--- public/js/signup.js
+++ public/js/signup.js
@@ -20,17 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
-    // document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-    //     e.preventDefault();
-    //     loginForm.classList.add("form--hidden");
-    //     createAccountForm.classList.remove("form--hidden");
-    // });
 
-    // document.querySelector("#linkLogin").addEventListener("click", e => {
-    //     e.preventDefault();
-    //     loginForm.classList.remove("form--hidden");
-    //     createAccountForm.classList.add("form--hidden");
-    // });
+
+
 
     // loginForm.addEventListener("submit", e => {
     //     e.preventDefault();
@@ -39,17 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //     setFormMessage(loginForm, "error", "Invalid username or password");
     // });
-
-    // createAccountForm.addEventListener("submit", e => {
-    //     e.preventDefault();
-
-    //     // Perform your AJAX/Fetch login
-
-    //     //setFormMessage(loginForm, "error", "Invalid username/password combination");
-    // });
-
-
-
 
 
 
@@ -89,3 +70,5 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
