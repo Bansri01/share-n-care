55f557b78240fe2791479808425900c05c78c82d
Pallavi Jaiswal <pallujaiswal0@gmail.com>
2021-12-03 00:45:57 -0400

    client side error handling in signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -37,33 +37,64 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 5) {
-                setInputError(inputElement, "Username must be at least 5 characters in length");
-            }
-            //password implement
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            // if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 5) {
+            //     setInputError(inputElement, "Username must be at least 5 characters in length");
+            // }
             
+            if (e.target.id === "firstName" && !e.target.value.firstName )  {
+                setInputError(inputElement, "You must provide profile First Name");
+              }
+            if (e.target.id === "lastName" && !e.target.value.lastName )  {
+                setInputError(inputElement, "You must provide profile Last Name");
+            }
+            if (e.target.id === "signupUsername" && !e.target.value.username )  {
+                setInputError(inputElement, "You must provide profile Username");
+            }
+            if (e.target.id === "emailAddress" && !e.target.value.emailAddress )  {
+                setInputError(inputElement, "You must provide profile Email Address");
+            }
+            if (e.target.id === "password" && !e.target.value.password )  {
+                setInputError(inputElement, "You must provide profile Password");
+            }   
+            if (e.target.id === "phoneNumber" && !e.target.value.phoneNumber )  {
+                setInputError(inputElement, "You must provide profile Phone Number");
+            } 
+            if (e.target.id === "country" && !e.target.value.country )  {
+                setInputError(inputElement, "You must provide profile Country");
+            } 
+            if (e.target.id === "Biography" && !e.target.value.biography )  {
+                setInputError(inputElement, "You must provide profile Biography");
+            }
+            if (e.target.id === "genderList" && !e.target.value.gender )  {
+                setInputError(inputElement, "You must provide Gender");
+            }
+            if (e.target.id === "userType" && !e.target.value.userType )  {
+                setInputError(inputElement, "You must provide UserType");
+            }
+            if (e.target.id === "dateOfBirth" && !e.target.value.dateOfBirth )  {
+                setInputError(inputElement, "You must provide Date of Birth");
+            } 
+            //password implement        
         });
+        // inputElement.addEventListener("blur", e => {
+            
+        //     if (e.target.id === "firstName" && (/^ *$/.test(e.target.value.firstName)))  {
+        //         setInputError(inputElement, "Firstname cannot be empty");
+        //       }
+                    
+        // });
+
+
+
+
+
+
+
+
+
+
+
+
 
         inputElement.addEventListener("input", e => {
             clearInputError(inputElement);
