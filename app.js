f359976357fd62ab5729b366d4a41caab2916f85
bansri01 <patelbansri987654321@gmail.com>
2021-12-12 13:26:17 -0500

    added middlewares

--- app.js
+++ app.js
@@ -62,18 +62,21 @@ app.use("*", (req, res, next) => {
   }
 });
 
-const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-  // If the user posts to the server with a property called _method, rewrite the request's method
-  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
-  // rewritten in this middleware to a PUT route
-  if (req.body && req.body._method) {
-    req.method = req.body._method;
-    delete req.body._method;
+app.use("/profile", (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  } else {
+    next();
   }
+});
 
-  // let the next middleware run:
-  next();
-};
+app.use("/searchProfile/:id", (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  } else {
+    next();
+  }
+})
 
 
 
